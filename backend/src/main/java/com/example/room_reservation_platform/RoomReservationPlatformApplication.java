6c81e5c55f4e10ba1aa3bffd687db901befbf75c
hojeong_s <hojeong1387@gmail.com>
2026-05-04 20:44:35 +0900

    docker DB 기본 세팅 설정

--- backend/src/main/java/com/example/room_reservation_platform/RoomReservationPlatformApplication.java
+++ backend/src/main/java/com/example/room_reservation_platform/RoomReservationPlatformApplication.java
@@ -2,15 +2,8 @@ package com.example.room_reservation_platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(
-		exclude = {
-				DataSourceAutoConfiguration.class,
-				HibernateJpaAutoConfiguration.class
-		}
-)
+@SpringBootApplication
 public class RoomReservationPlatformApplication {
 
 	public static void main(String[] args) {
--- backend/src/test/java/com/example/room_reservation_platform/RoomReservationPlatformApplicationTests.java
+++ backend/src/test/java/com/example/room_reservation_platform/RoomReservationPlatformApplicationTests.java
@@ -3,7 +3,14 @@ package com.example.room_reservation_platform;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+		properties = {
+				"spring.autoconfigure.exclude=" +
+						"org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration," +
+						"org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration," +
+						"org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration"
+		}
+)
 class RoomReservationPlatformApplicationTests {
 
 	@Test
